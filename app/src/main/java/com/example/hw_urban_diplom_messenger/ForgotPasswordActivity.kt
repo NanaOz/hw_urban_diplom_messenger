7e9d11a382065db86a1382bee7c77ab627c65e12
Alina <alinavik93@gmail.com>
2024-05-24 19:23:01 +0500

    Сделан сброс пароля

--- app/src/main/java/com/example/hw_urban_diplom_messenger/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/hw_urban_diplom_messenger/ForgotPasswordActivity.kt
@@ -2,10 +2,38 @@ package com.example.hw_urban_diplom_messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.hw_urban_diplom_messenger.databinding.ActivityForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+
+            if (email.isNotEmpty()) {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Password reset email sent.", Toast.LENGTH_SHORT)
+                                .show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Failed to send password reset email.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
