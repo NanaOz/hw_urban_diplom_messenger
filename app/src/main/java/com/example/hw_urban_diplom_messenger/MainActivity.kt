120f4452c78d0749b8227a713a5367e264a1b8e8
Alina <alinavik93@gmail.com>
2024-06-14 16:02:19 +0500

    Главный экран

--- app/src/main/java/com/example/hw_urban_diplom_messenger/MainActivity.kt
+++ app/src/main/java/com/example/hw_urban_diplom_messenger/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hw_urban_diplom_messenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import com.example.hw_urban_diplom_messenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +15,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.logInButton.setOnClickListener {
+        val slideBlinkAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_main)
+        binding.imageView.startAnimation(slideBlinkAnimation)
+
+        Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }
+            finish()
+        }, 3000)
     }
 }
